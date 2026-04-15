941dc5520a76623dc3267cc4a60142c44fe990dd
krutharth-dev <krutharthpgowda08@gmail.com>
2026-04-15 21:03:01 +0530

    Day 1: Python basics (input, math, temp conversion)

---
+++ day1/basics.py
@@ -0,0 +1,14 @@
+print("Hello, I am learning Python 🚀")
+
+name = input("Enter your name: ")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum:", a + b)
+print("Multiplication:", a * b)
+
+c = float(input("Enter temperature in Celsius: "))
+f = (c * 9/5) + 32
+
+print("Hello", name, ", temperature in Fahrenheit is", f)
